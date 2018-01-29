b0ce9a7bc0e3a5464fec67450d9fedca73919b71
Frag Spawn <frag.spawn@gmail.com>
2018-01-29 16:25:22 +1000

    email check

--- client/registration_check_email.php
+++ client/registration_check_email.php
@@ -1,16 +1,17 @@
 <?php
-
-    $select_sql = "SELECT email from users where email = '" . $_POST['email'] . "';";
+    $select_sql = "SELECT email from users where email = '" . $_GET['email'] . "';";
 
     $conn = new PDO("mysql:host=localhost;dbname=GQS", 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     $stmt = $conn->prepare($select_sql);
     $stmt->execute();
 
     $result = $stmt->fetchAll();
 
-    if(is_array($result)) {
-        echo 'true';
+    if(count($result) > 0) {
+        echo "red text";
     } else {
-        echo 'false';
+        echo "green tick";
     }
 ?>
--- client/registration_process.php
+++ client/registration_process.php
@@ -4,12 +4,6 @@
                     ( '" . $_POST['fname'] . "',  '" . $_POST['lname'] . "',  '" . 
                     $_POST['email'] . "',  '" .$_POST['phone'] . "');";
 
-    echo $insert_sql;
-    die();
-
-    $select_sql = "SELECT email from users where email = '" . $_POST['email'] . "';";
-
-
     $conn = new PDO("mysql:host=localhost;dbname=GQS", 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
