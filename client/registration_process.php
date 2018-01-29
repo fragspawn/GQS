b4ce599bc444a91d7e845e7f3c8449ef0b3ca716
Frag Spawn <frag.spawn@gmail.com>
2018-01-29 13:20:30 +1000

    SQL statements

--- client/registration_process.php
+++ client/registration_process.php
@@ -1,6 +1,10 @@
 <?
-    $insert_sql = "INSERT "; 
-// INSERT INTO  `GQS`.`users` ( `ID` , `fname` , `lname` , `email` , `phone`) VALUES ( NULL ,  'asdf',  'asdf',  'asdf',  'asdf');
+
+    $insert_sql = "INSERT INTO  users (fname, lname, email, phone) VALUES 
+                    ( '" . $_POST['fname'] . "',  '" . $_POST['lname'] . "',  '" . 
+                    $_POST['email'] . "',  '" .$_POST['phone'] . "');";
+
     $select_sql = "SELECT email from users where email = '" . $_POST['email'] . "';";
-    print_r($_POST);
+    echo '<p>' . $select_sql . '</p>';
+    echo '<p>' . $insert_sql . '</p>';
 ?>
