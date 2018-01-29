f0c705300bade78cf209adb2ddf1a280436eb8e0
Frag Spawn <frag.spawn@gmail.com>
2018-01-29 12:37:19 +1000

    sql

--- client/registration_process.php
+++ client/registration_process.php
@@ -1,3 +1,6 @@
 <?
-    print_r($_POSt);
+    $insert_sql = "INSERT "; 
+// INSERT INTO  `GQS`.`users` ( `ID` , `fname` , `lname` , `email` , `phone`) VALUES ( NULL ,  'asdf',  'asdf',  'asdf',  'asdf');
+    $select_sql = "SELECT email from users where email = '" . $_POST['email'] . "';";
+    print_r($_POST);
 ?>
