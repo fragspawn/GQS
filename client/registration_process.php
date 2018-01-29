1bd0a4c068926469e0d010549b92d82c12b6a49b
Frag Spawn <frag.spawn@gmail.com>
2018-01-29 13:25:17 +1000

    SQL fixes

--- client/registration_process.php
+++ client/registration_process.php
@@ -1,10 +1,11 @@
-<?
+<?php
 
     $insert_sql = "INSERT INTO  users (fname, lname, email, phone) VALUES 
                     ( '" . $_POST['fname'] . "',  '" . $_POST['lname'] . "',  '" . 
                     $_POST['email'] . "',  '" .$_POST['phone'] . "');";
 
     $select_sql = "SELECT email from users where email = '" . $_POST['email'] . "';";
-    echo '<p>' . $select_sql . '</p>';
-    echo '<p>' . $insert_sql . '</p>';
+
+    print '<p>' . $select_sql . '</p>';
+    print '<p>' . $insert_sql . '</p>';
 ?>
